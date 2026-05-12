84bc202978d7751876fc4e5b6c64a9a8aa3f83fe
Sumit Kumar <sumit.1183@zenmonk.tech>
2026-05-12 14:43:09 +0530

    seeding fixed by inserting id and uuid same across microservices

--- main-server/src/infrastructure/database/seeder.seed.ts
+++ main-server/src/infrastructure/database/seeder.seed.ts
@@ -6,21 +6,96 @@ import { UserRoleEnum } from 'src/domain/user/user.enum';
 
 // hardcoded creators for all microservices
 const creators = [
-    { email: 'creator1@test.com', name: 'Creator 1' },
-    { email: 'creator2@test.com', name: 'Creator 2' },
-    { email: 'creator3@test.com', name: 'Creator 3' },
-    { email: 'creator4@test.com', name: 'Creator 4' },
-    { email: 'creator5@test.com', name: 'Creator 5' },
-    { email: 'creator6@test.com', name: 'Creator 6' },
-    { email: 'creator7@test.com', name: 'Creator 7' },
-    { email: 'creator8@test.com', name: 'Creator 8' },
-    { email: 'creator9@test.com', name: 'Creator 9' },
-    { email: 'creator10@test.com', name: 'Creator 10' },
-    { email: 'creator11@test.com', name: 'Creator 11' },
-    { email: 'creator12@test.com', name: 'Creator 12' },
-    { email: 'creator13@test.com', name: 'Creator 13' },
-    { email: 'creator14@test.com', name: 'Creator 14' },
-    { email: 'creator15@test.com', name: 'Creator 15' },
+    {
+        id: 1,
+        uuid: '11111111-1111-1111-1111-111111111111',
+        email: 'creator1@gmail.com',
+        name: 'Creator 1',
+    },
+    {
+        id: 2,
+        uuid: '22222222-2222-2222-2222-222222222222',
+        email: 'creator2@gmail.com',
+        name: 'Creator 2',
+    },
+    {
+        id: 3,
+        uuid: '33333333-3333-3333-3333-333333333333',
+        email: 'creator3@gmail.com',
+        name: 'Creator 3',
+    },
+    {
+        id: 4,
+        uuid: '44444444-4444-4444-4444-444444444444',
+        email: 'creator4@gmail.com',
+        name: 'Creator 4',
+    },
+    {
+        id: 5,
+        uuid: '55555555-5555-5555-5555-555555555555',
+        email: 'creator5@gmail.com',
+        name: 'Creator 5',
+    },
+    {
+        id: 6,
+        uuid: '66666666-6666-6666-6666-666666666666',
+        email: 'creator6@gmail.com',
+        name: 'Creator 6',
+    },
+    {
+        id: 7,
+        uuid: '77777777-7777-7777-7777-777777777777',
+        email: 'creator7@gmail.com',
+        name: 'Creator 7',
+    },
+    {
+        id: 8,
+        uuid: '88888888-8888-8888-8888-888888888888',
+        email: 'creator8@gmail.com',
+        name: 'Creator 8',
+    },
+    {
+        id: 9,
+        uuid: '99999999-9999-9999-9999-999999999999',
+        email: 'creator9@gmail.com',
+        name: 'Creator 9',
+    },
+    {
+        id: 10,
+        uuid: 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
+        email: 'creator10@gmail.com',
+        name: 'Creator 10',
+    },
+    {
+        id: 11,
+        uuid: 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb',
+        email: 'creator11@gmail.com',
+        name: 'Creator 11',
+    },
+    {
+        id: 12,
+        uuid: 'cccccccc-cccc-cccc-cccc-cccccccccccc',
+        email: 'creator12@gmail.com',
+        name: 'Creator 12',
+    },
+    {
+        id: 13,
+        uuid: 'dddddddd-dddd-dddd-dddd-dddddddddddd',
+        email: 'creator13@gmail.com',
+        name: 'Creator 13',
+    },
+    {
+        id: 14,
+        uuid: 'eeeeeeee-eeee-eeee-eeee-eeeeeeeeeeee',
+        email: 'creator14@gmail.com',
+        name: 'Creator 14',
+    },
+    {
+        id: 15,
+        uuid: 'ffffffff-ffff-ffff-ffff-ffffffffffff',
+        email: 'creator15@gmail.com',
+        name: 'Creator 15',
+    },
 ];
 
 async function create() {
@@ -43,6 +118,8 @@ async function create() {
         // use same creators across all services
         for (const creator of creators) {
             const user = await queryRunner.manager.save(UserEntity, {
+                id: creator.id,
+                uuid: creator.uuid,
                 email: creator.email,// faker.internet.email(),
                 password: hashedPassword,
                 name: creator.name,// faker.person.fullName(),
